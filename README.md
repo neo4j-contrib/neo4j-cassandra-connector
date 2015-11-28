9f20ba1f2ae0e2a16ab152257b2e6d789823c82d
hannelita <hannelita@gmail.com>
2015-11-27 23:49:12 -0200

    Adding information on README and refactoring namedtuples

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Neo4j Cassandra Connector
+
+Welcome to [Neo4j](http://neo4j.com/) [Cassandra](http://cassandra.apache.org/) Connector. The main goal of this project is to import a specific Tracks dataset that is in a Cassandra to a graph structure in Neo4j. 
+
+Please be aware that this project is still under development and at this first stage it will work mainly for this restricted data collection. Please check our [docs](docs/cassandra_connector_doc.adoc) to have more information of the covered scenarios and a detailed instruction set for using this tool. 
--- connector/cypher_queries_generator.py
+++ connector/cypher_queries_generator.py
@@ -1,5 +1,6 @@
 import re
 import os
+from collections import namedtuple
 from node_structure import NodeStructure
 
 class CypherQueriesGenerator(object):
@@ -24,15 +25,45 @@ class CypherQueriesGenerator(object):
       self.collect_filled_parameters(line)
     return self.nodes
 
-  # def build_queries(self, nodes, [track_by_id, artists_by_first_letter], ["music_results.csv", "artists_names_results.csv"])
   def build_queries(self, nodes, tables, result_csvs):
     cypher_file = open('cypher_', 'w+')
     columns = self.analyse_csv(result_csvs)
-    info_tuple = zip(tables, result_csvs, columns)
-    print(info_tuple)
-    # for idx, unique_name in enumerate(tables):
-    #   node = self.get_node(unique_name, nodes)
-      # query = "LOAD CSV FROM " + result_csvs[idx] + "AS LINE CREATE (n" + node.label + "{ first_letter: line[0], artist: line[1] } );"
+    nodes_structure = self.analyse_node(tables, nodes)
+    # builds a tuple with all needed information to build a Cypher query
+    Builder = namedtuple('Builder', 'node_name csv_file column_size node_structure')
+    info_tuple = zip(tables, result_csvs, columns, nodes_structure)
+    for element in info_tuple:
+      element = Builder(element[0], element[1], element[2], element[3])
+      params_hash = self.build_params_hash(element.node_structure, element.column_size)
+      if(element[3].relationships):
+        relationships_query = self.build_relationships(element.node_structure)
+        print(relationships_query)
+      #AS LINE MATCH (b:playlist:artist) WHERE b.artist=line[1] 
+      query = "LOAD CSV FROM {csv_file} AS LINE CREATE (n{label} {params} );\n".format(csv_file=element.csv_file, label=element.node_structure.label, params=str(params_hash))
+      cypher_file.write(query)
+    
+  def build_relationships(self, node):
+    idx = 0
+    key = ""
+    for rel in node.relationships.keys():
+      idx = node.properties.keys().index(rel)
+      key = rel
+    query = "AS LINE MATCH (n{label} WHERE n. {key}=line[{idx}] ".format(label=node.label, key=key, idx=idx)
+    return query
+
+  def analyse_node(self, tables, nodes):
+    nodes_structure = []
+    for idx, unique_name in enumerate(tables):
+      node = self.get_node(unique_name, nodes)
+      nodes_structure.append(node)
+    return nodes_structure
+
+  def build_params_hash(self, node, size):
+    params = {}
+    for i in range(size):
+      params.update({node.properties.keys()[i]: "line["+ str(i) +"]"})
+    return params
+
 
   def analyse_csv(self, result_csvs):
     columns = []
@@ -59,8 +90,7 @@ class CypherQueriesGenerator(object):
     filled_parameter = re.match(".*\{(.*)\}.*", line)
     if(filled_parameter):
       data = (line.lstrip()).split(":")[0]
-      if(filled_parameter.group(1) == "p"):
-        self.node.properties.update({data.split(" ")[0]:data.split(" ")[1]})
-      else:
+      if(filled_parameter.group(1) == "r"):
         self.node.relationships.update({data.split(" ")[0]:data.split(" ")[1]})
+      self.node.properties.update({data.split(" ")[0]:data.split(" ")[1]})
 
