2fcd5620cf5a8b0a8fa6805e57fcefc5c44f5d52
johnymontana <lyonwj@gmail.com>
2015-12-17 15:31:38 -0800

    Add commandline args for running Cassandra Connector
    Add CassandraConnector class
    Update README

--- README.md
+++ README.md
@@ -2,4 +2,21 @@
 
 Welcome to [Neo4j](http://neo4j.com/) [Cassandra](http://cassandra.apache.org/) Connector. The main goal of this project is to import a specific Tracks dataset that is in a Cassandra to a graph structure in Neo4j. 
 
-Please be aware that this project is still under development and at this first stage it will work mainly for this restricted data collection. Please check our [docs](docs/cassandra_connector_doc.adoc) to have more information of the covered scenarios and a detailed instruction set for using this tool. 
+Please be aware that this project is still under development and at this first stage it will work mainly for this restricted data collection. Please check our [docs](docs/cassandra_connector_doc.adoc) to have more information of the covered scenarios and a detailed instruction set for using this tool.
+
+## Example usage
+
+This example shows how to use the Cassandra Neo4j Connector to convert the music playlist dataset from Cassandra into Neo4j:
+
+1. Ensure Cassandra and Neo4j are running
+1. Clone this repository: `git clone https://github.com/neo4j-contrib/neo4j-cassandra-connector.git`
+1. `cd neo4j-cassandra-connector`
+1. `pip install -r requirements.txt` - note only Python 3.4+ is currently supported
+1. `python connector/connector.py parse -k playlist` - this will parse the schema metadata for the specified Cassandra keyspace and generate a file `schema.yaml`. Edit this file to specify the graph structure you would like to use to import your data. See example here. An example is provided: `cp schema.yaml.example schema.yaml`
+1. `python connector/connector.py export -k KEYSPACE_NAME_HERE -t track_by_id,artists_by_first_letter` - this will export the data from Cassandra using CSV as an intermediary and import the data into Neo4j per the specified graph data model.
+
+## TODO
+
+[ ] Allow user specified CQL queries
+[ ] Python 2.7 support
+[ ] More robust graph model configuration
--- connector/connector.py
+++ connector/connector.py
@@ -1,10 +1,10 @@
-
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 from cassandra.cluster import Cluster
 from cassandra.metadata import Metadata
 from cassandra.metadata import KeyspaceMetadata
+from cassandra.metadata import TableMetadata
 import re
 import sys
 from io import open
@@ -12,33 +12,83 @@ from schema_parser import SchemaParser
 from cypher_queries_generator import CypherQueriesGenerator
 import csv
 import codecs
+import argparse
 
+class CassandraConnector(object):
+  """
+    Handles exporting data from Cassandra to Neo4j
+    using CSV as intermediary
 
-cluster = Cluster()
-session = cluster.connect('playlist')
-meta_str = session.cluster.metadata.export_schema_as_string()
-keyspace = session.cluster.metadata.keyspaces["playlist"]
-if(len(sys.argv) <= 1):
-  parser = SchemaParser(keyspace.export_as_string())
-  parser.parse()
-  sys.exit("Generated schema.yaml file.")
+  """
 
-music_results_file = codecs.open('music_results.csv', encoding='utf-8', mode='w+')
-rows = session.execute('SELECT * FROM track_by_id')
-writer = csv.writer(music_results_file)
-writer.writerow(['track_id', 'artist', 'genre', 'music_file', 'track', 'track_length_in_seconds'])
-writer.writerows([(track.track_id, track.artist, track.genre, track.music_file, track.track, track.track_length_in_seconds) for track in rows])
+  def __init__(self, keyspace_name, tables=None, queries=None):
+    self.KEYSPACE = keyspace_name
+    self.session = Cluster().connect(self.KEYSPACE)
+    self.keyspace_metadata = self.session.cluster.metadata.keyspaces[self.KEYSPACE]
+    self.tables = tables
 
-artists_names_results_file = codecs.open('artists_names_results.csv', encoding='utf-8', mode='w+')
-rows = session.execute('SELECT * FROM artists_by_first_letter')
-writer = csv.writer(artists_names_results_file)
-writer.writerow(['first_letter', 'artist'])
-writer.writerows([(artistt.first_letter, artistt.artist) for artistt in rows])
+  def getTables(self):
+    if self.tables:
+      return self.tables
+    else:
+      return self.session.cluster.metadata.keyspaces[self.KEYSPACE].tables.keys()
 
-cypher_queries_gen = CypherQueriesGenerator(keyspace)
-cypher_queries_gen.generate()
-cypher_queries_gen.build_queries(["track_by_id", "artists_by_first_letter"], ["music_results.csv", "artists_names_results.csv"])
+  def getColumnsForTable(self, table):
+    return self.session.cluster.metadata.keyspaces[self.KEYSPACE].tables[table].columns.keys()
 
+  def parse(self):
+    keyspace = self.session.cluster.metadata.keyspaces[self.KEYSPACE]
+    parser = SchemaParser(keyspace.export_as_string())
+    parser.parse()
+    sys.exit("Generated schema.yaml file.")
+
+  def export(self):
+    tableNames = self.getTables()
+    fileNames = [t + "_results.csv" for t in tableNames]
+    print(tableNames)
+    print(fileNames)
+
+    for t in tableNames:
+      results_file = codecs.open(t + "_results.csv", encoding='utf-8', mode='w+')
+      rows = self.session.execute('SELECT * FROM ' + t)
+      writer = csv.writer(results_file)
+      writer.writerow(self.getColumnsForTable(t))
+      writer.writerows([(e for e in row) for row in rows])
+
+    cypher_queries_gen = CypherQueriesGenerator(self.keyspace_metadata)
+    cypher_queries_gen.generate()
+    cypher_queries_gen.build_queries(tableNames, fileNames)
+
+class Neo4jNodeBuilder(object):
+  def __init__(self, keyspace, schema):
+    self.keyspace = keyspace
+    self.schema = schema
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("action", type=str, help="Specify action, 'parse' to generate schema.yaml file. Then 'export' to export to Neo4j.", choices=["parse", "export"])
+  parser.add_argument("-k", "--keyspace", type=str, help="Specify the Cassandra keyspace. If no keyspace is specified then 'playlist' will be used by default")
+  parser.add_argument("-t", "--tables", type=str, help="Specify Cassandra table(s) to export to Neo4j in a comma separated list. If not specified then all tables will be exported.")
+
+  args=parser.parse_args()
+  keyspace = ''
+  if args.keyspace:
+    keyspace = args.keyspace
+  else:
+    keyspace = 'playlist'
+
+  if args.action == "parse":
+    connector = CassandraConnector(keyspace)
+    connector.parse()
+  elif args.action == 'export':
+    if args.tables:
+      tables = args.tables.split(",")
+      print(tables)
+      connector = CassandraConnector(keyspace, tables=tables)
+    else:
+      connector = CassandraConnector(keyspace)
+
+    connector.export()
 
 class Neo4jNodeBuilder(object):
   def __init__(self, keyspace, schema):
